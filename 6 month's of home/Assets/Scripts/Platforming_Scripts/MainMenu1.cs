5cf4706d81e6121d79e2ade576b6c8e57e555cdf
Unknown <bjcox93@gmail.com>
2018-11-28 14:40:16 +1300

    Beat_V9

--- 6 month's of home/Assets/Scripts/Platforming_Scripts/MainMenu1.cs
+++ 6 month's of home/Assets/Scripts/Platforming_Scripts/MainMenu1.cs
@@ -5,20 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu1 : MonoBehaviour {
 
-    
+    public GameObject StartNewGO;
+    public Transform Target1StartNew; 
+    public Transform OriginalTargetStartNew;
+
+    public GameObject ExitGO;
+    public Transform Target1Exit;
+    public Transform OriginalTargetExit;
+
+    public GameObject ContinueGO;
+    public Transform Target1Continue;
+    public Transform OriginalTargetContinue;
+
+    public GameObject OptionsGO;
+    public Transform Target1Options;
+    public Transform OriginalTargetOptions;
+
+    public float SpeedOfMove;
 
     public GameObject MainMenuUI;
 
 	// Use this for initialization
 	void Start () {
         Reset.instance.ScoreCanvas.SetActive(true);
+        
     }
-	
-	// Update is called once per frame
-	
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //float Step = SpeedOfMove * Time.deltaTime;
+    }
 
     public void StartNew()
     {
+        StartNewGO.transform.position = Vector3.MoveTowards(transform.position, Target1StartNew.position, SpeedOfMove * Time.deltaTime); // if shape to target load level bool = true. If load level bool == true then load level.
         SceneManager.LoadScene("LEVEL_2");
     }
 
@@ -26,11 +47,13 @@ public class MainMenu1 : MonoBehaviour {
 
     public void OptionsMenu()
     {
+        OptionsGO.transform.position = Vector3.MoveTowards(transform.position, Target1Options.position, SpeedOfMove * Time.deltaTime);
         SceneManager.LoadScene("Title_Menu_Options");
     }
 
     public void LoadLevelLoadMenu()
     {
+
         SceneManager.LoadScene("Title_Menu_LOAD_LEVEL");
     }
 
@@ -41,6 +64,7 @@ public class MainMenu1 : MonoBehaviour {
 
     public void QuitGame()
     {
+        ExitGO.transform.position = Vector3.MoveTowards(transform.position, Target1Exit.position, SpeedOfMove * Time.deltaTime);
         Debug.Log("QUIT");
         Application.Quit();
         
