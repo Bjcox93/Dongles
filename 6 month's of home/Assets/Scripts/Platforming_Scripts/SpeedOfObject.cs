f108c6692574a032ab0c155786ccbaa2efacb113
Bradley Cox <bjcox93@gmail.com>
2019-03-01 08:14:27 +1300

    Beat 88
    Speed fixed

--- 6 month's of home/Assets/Scripts/Platforming_Scripts/SpeedOfObject.cs
+++ 6 month's of home/Assets/Scripts/Platforming_Scripts/SpeedOfObject.cs
@@ -8,7 +8,7 @@ public class SpeedOfObject : MonoBehaviour {
 
     public float timeP = 2.0f;
     public float timeO = 2.0f;
-    public float timeF = 3.0f;
+    public float timeF = 30.0f;
 
     // Use this for initialization
     void Start () {
@@ -51,7 +51,7 @@ public class SpeedOfObject : MonoBehaviour {
                 {
                     Debug.Log("Hot");
                     Reset.instance.ResetFun();
-                    timeF = 3.0f;
+                    timeF = 30.0f;
                 }
             }
         }
